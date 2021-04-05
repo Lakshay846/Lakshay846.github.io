b2374d5ca7cfdb43f1f5b15c13107c586aecc32a
Lakshay Kumar <lakshkumar846@gmail.com>
2021-04-05 11:03:52 +0530

    updated minicart

--- js/minicart.js
+++ js/minicart.js
@@ -1915,7 +1915,7 @@ var defaults = module.exports = {
 
     strings: {
         //button: 'Check Out with <img src="//cdnjs.cloudflare.com/ajax/libs/minicart/3.0.1/paypal_65x18.png" width="65" height="18" alt="PayPal" />',
-        button: '<a href="checkout.html" id="checkout">Check Out with</a>',
+        button: `<a href="checkout.html" id="checkout">Check Out with</a>`,
         subtotal: 'Subtotal:',
         discount: 'Discount:',
         empty: 'Your shopping cart is empty'
