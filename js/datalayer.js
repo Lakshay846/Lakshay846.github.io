985adeaad074221a718553dcc383706fbca13616
Lakshay Kumar <43853851+Lakshay846@users.noreply.github.com>
2022-08-04 13:20:34 +0530

    Added datalayer

--- js/datalayer.js
+++ js/datalayer.js
@@ -1,12 +1,54 @@
 //DATA LAYER-->
-		var digitalData = {
-			"page": {
-				"pageInfo" : {
-					"pageName": location.pathname
-					}
-				},
-				product: []
-		};
+	var pagename = document.title;
+	var url = document.URL;
+	var host = window.location.host;
+	var d1 = new Date(); 
+	var currDate = d1.toISOString().slice(0, 10);
+	var currTime = d1.toLocaleTimeString();
+	var currDevice = "";
+	if(screen.width <= 421)
+		currDevice = "Mobile";
+	else if (screen.width > 421 && screen.width <= 1023)
+		currDevice = "Tablet";
+	else
+		currDevice = "Desktop";
+
+	localStorage.setItem("isLoggedIn", false);	
+
+	setInterval(() => {
+		d1 = new Date()
+		currTime = d1.toLocaleTimeString()
+		digitalData.attributes.time = currTime;
+	}, 1000);
+
+	var digitalData = {
+			page: {
+				pageName: pagename,
+				pageURL : url,
+				deviceType: currDevice,
+				page_level: 3,
+				loggedin: JSON.parse(localStorage.getItem('isLoggedIn')),
+			},
+			category: {
+				primaryCategory: "Login",
+				subCategory1: "",
+				subCategory2: "",
+				subCategory3: "",
+
+			},
+			attributes: {
+				country: "IN",
+				date: currDate,
+				time: currTime,
+				domain: host
+			},
+			user:{
+				userID : "lakshay846"
+			},
+			product : []
+	}
+
+
 	function test(){
 		$(".snipcart-details").click(function(){
 			var i = digitalData.product.length;
